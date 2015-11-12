501ed3a312601c1e6bf745e1cf52cef6117a7aca
dmitri <deemok@gmail.com>
2015-11-12 03:40:43 +0100

    planet/monit: working monit scripts and a running service
    Changed monit supervisor scripts to watch processes by mask thus
    avoiding the need for pid files. Monit configurations scripts as well as
    runtime data have been assigned a single directory: /etc/monit.
    Added a basic alerts file to keep alerts as json records.

--- build.assets/makefiles/master/monit/monit.mk
+++ build.assets/makefiles/master/monit/monit.mk
@@ -3,7 +3,7 @@
 # TODO: build monit from source
 # https://bitbucket.org/tildeslash/monit/
 
-BINARIES := $(TARGETDIR)/monit
+BINARIES := $(ROOTFS)/lib/monit/init/monitrc
 
 all: monit.mk $(BINARIES)
 
@@ -14,5 +14,6 @@ $(BINARIES):
 	ln -sf /lib/systemd/system/monit.service  $(ROOTFS)/lib/systemd/system/multi-user.target.wants/
 	install -m 0755 $(TARGETDIR)/monit $(ROOTFS)/usr/bin
 	mkdir -p $(ROOTFS)/lib/monit/init
+	mkdir -p $(ROOTFS)/etc/monit
 	cp ./*.conf $(ROOTFS)/lib/monit/init
-	install -m 0644 ./monitrc $(ROOTFS)/lib/monit/init/
+	install -m 0755 ./monitrc $(ROOTFS)/lib/monit/init/
---
+++ tool/planet/alert.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gravitational/planet/Godeps/_workspace/src/github.com/gravitational/log"
+	"github.com/gravitational/planet/Godeps/_workspace/src/github.com/gravitational/trace"
+)
+
+const alertsFile = "/etc/monit/alerts"
+
+type alertMessage struct {
+	Timestamp time.Time `json:"timestamp"`
+	Module    string    `json:"message"`
+	Reason    string    `json:"reason"`
+}
+
+// alert adds an alert message for `module` to alerts file.
+func alert(module, reason string) error {
+	var message []byte
+	var f *os.File
+	var err error
+	item := &alertMessage{
+		Timestamp: time.Now().UTC(),
+		Module:    module,
+		Reason:    reason,
+	}
+
+	f, err = os.OpenFile(alertsFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return trace.Wrap(err, fmt.Sprintf("cannot open alerts file `%s`", alertsFile))
+	}
+	defer f.Close()
+
+	message, err = json.Marshal(item)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	log.Infof(item.String())
+	_, err = f.Write(message)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	return nil
+}
+
+func (r *alertMessage) String() string {
+	return fmt.Sprintf("alert(module=%s, reason=%s, when=%s)", r.Module, r.Reason, r.Timestamp)
+}
