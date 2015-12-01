467a0166356b5fc53967724499dff489033d6c74
dmitri <deemok@gmail.com>
2015-12-01 02:59:21 +0100

    planet: replace registry running as docker container with a native image.

--- build.assets/makefiles/registry/registry.mk
+++ build.assets/makefiles/registry/registry.mk
@@ -1,6 +1,13 @@
 .PHONY: all
 
+# TODO: build registry
+VER := 2.2.0
+
 all: registry.mk
 	@echo "\\n---> Creating registry service\\n"
 	cp -af ./registry.service $(ROOTFS)/lib/systemd/system
 	ln -sf /lib/systemd/system/registry.service  $(ROOTFS)/lib/systemd/system/multi-user.target.wants/
+	mkdir -p $(ROOTFS)/usr/bin
+	cp $(ASSETS)/docker/registry/registry $(ROOTFS)/usr/bin/
+	mkdir -p $(ROOTFS)/etc/docker/registry
+	cp $(ASSETS)/docker/registry/config.yml $(ROOTFS)/etc/docker/registry/
