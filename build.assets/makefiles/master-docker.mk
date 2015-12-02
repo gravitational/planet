c294e927f1a34414d4c0fb51df8eb7dbcb418d86
dmitri <deemok@gmail.com>
2015-12-02 02:25:05 +0100

    planet: replace docker registry container with a native binary.
    Removed the now redundant precompiled registry binary, fixed
    whitespace and used a stricter program name in monit configuration.

--- build.assets/makefiles/master-docker.mk
+++ build.assets/makefiles/master-docker.mk
@@ -8,7 +8,7 @@ all: planet-bin
 # common components:
 	make -C $(ASSETS)/makefiles/base/network -f network.mk
 	make -C $(ASSETS)/makefiles/base/docker -f docker.mk 
-	make -C $(ASSETS)/makefiles/base/docker -f registry.mk 
+	make -C $(ASSETS)/makefiles/base/docker -f registry.mk
 	make -C $(ASSETS)/makefiles/kubernetes -f kubernetes.mk
 	make -C $(ASSETS)/makefiles/monit -f monitoring.mk
 # master-image specific:
