b37797c3b849b61a483cc3834a53068a1bf11d91
dmitri <deemok@gmail.com>
2015-12-11 00:48:16 +0100

    Fixed a typo in strconv.FormatInt call.

--- lib/box/socket.go
+++ lib/box/socket.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"syscall"
 
 	"github.com/gravitational/planet/Godeps/_workspace/src/github.com/gravitational/log"
 	"github.com/gravitational/planet/Godeps/_workspace/src/github.com/gravitational/trace"
@@ -19,29 +20,30 @@ const SD_LISTEN_FDS_START uintptr = 3
 func newSockListener(rootfs string) (net.Listener, error) {
 	listener, err := fileListener()
 	if err == nil && listener != nil {
+		log.Infof("using socket activation")
 		return listener, nil
 	}
 	if err != nil {
-		return nil, trace.Wrap(err, "failed to use socket activation environment")
+		return nil, trace.Wrap(err, "failed to use socket activation context")
 	}
 
-	log.Infof("no socket activation environment - fall back to Listen")
+	log.Infof("no socket activation context - fall back to Listen")
 	listener, err = net.Listen("unix", serverSockPath(rootfs))
 	return listener, err
 }
 
 func fileListener() (net.Listener, error) {
-	listenPid := os.Getenv("LISTEN_PID")
-	if listenPid == "" || strconv.FormatInt(int64(os.Getpid()), 32) != listenPid {
+	listenPid, exists := syscall.Getenv("LISTEN_PID")
+	if !exists || strconv.FormatInt(int64(os.Getpid()), 10) != listenPid {
 		return nil, nil
 	}
-	listenFds := os.Getenv("LISTEN_FDS")
-	if listenFds == "" {
-		return nil, nil
+	listenFds, exists := syscall.Getenv("LISTEN_FDS")
+	if !exists {
+		return nil, trace.Errorf("invalid socket activation context - expected a socket file descriptor")
 	}
 	numFds, err := strconv.Atoi(listenFds)
 	if err != nil {
-		return nil, trace.Wrap(err, "invalid number of listen fds: %s (%v)", listenFds, err)
+		return nil, trace.Wrap(err, "invalid number of socket file descriptors: %s (%v)", listenFds, err)
 	}
 	if numFds <= 0 {
 		return nil, nil
@@ -53,6 +55,6 @@ func fileListener() (net.Listener, error) {
 	return listener, nil
 }
 
-func serverSockPath(p string) string {
-	return filepath.Join(p, "run", "planet.socket")
+func serverSockPath(rootfs string) string {
+	return filepath.Join(rootfs, "run", "planet.socket")
 }
