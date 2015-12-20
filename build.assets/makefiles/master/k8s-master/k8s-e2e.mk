a34617b1f9b6b83ded1c4d309a1912a87490d644
dmitri <deemok@gmail.com>
2015-12-20 22:08:15 +0100

    Fixing the problem in migration to versioned artefacts - copy e2e test artefacts from the proper location.

--- build.assets/makefiles/master/k8s-master/k8s-e2e.mk
+++ build.assets/makefiles/master/k8s-master/k8s-e2e.mk
@@ -2,5 +2,5 @@
 
 all: k8s-e2e.mk
 	@echo "\n---> Placing e2e binaries to rootfs"
-	install -m 0755 $(ASSETDIR)/e2e.test $(ROOTFS)/usr/bin
-	install -m 0755 $(ASSETDIR)/ginkgo $(ROOTFS)/usr/bin
+	install -m 0755 $(ASSETDIR)/k8s-$(KUBE_VER)/e2e.test $(ROOTFS)/usr/bin
+	install -m 0755 $(ASSETDIR)/k8s-$(KUBE_VER)/ginkgo $(ROOTFS)/usr/bin
