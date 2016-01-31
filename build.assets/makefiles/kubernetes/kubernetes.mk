1ddbc462dd86af4c0ca5b6187eef6d27bd2bd0c2
dmitri <deemok@gmail.com>
2016-01-31 18:44:37 +0100

    Build and import infra-POD container image offline

--- build.assets/makefiles/kubernetes/kubernetes.mk
+++ build.assets/makefiles/kubernetes/kubernetes.mk
@@ -9,7 +9,8 @@ BINARIES := $(OUTPUTDIR)/kube-apiserver \
 	$(OUTPUTDIR)/kube-proxy \
 	$(OUTPUTDIR)/kubelet \
 	$(OUTPUTDIR)/ginkgo \
-	$(OUTPUTDIR)/e2e.test
+	$(OUTPUTDIR)/e2e.test \
+	$(OUTPUTDIR)/pause
 
 all: kubernetes.mk $(BINARIES)
 
@@ -20,6 +21,8 @@ $(BINARIES):
 	mkdir -p $(GOPATH)/src/github.com/kubernetes
 	cd $(GOPATH)/src/github.com/kubernetes && git clone https://github.com/kubernetes/kubernetes -b $(KUBE_VER) --depth 1 
 	cd $(REPODIR) && ./hack/build-go.sh
+	cd $(REPODIR)/build/pause && CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w' ./pause.go
 	cp $(REPODIR)/_output/local/bin/linux/amd64/kube* $(OUTPUTDIR)/
 	cp $(REPODIR)/_output/local/bin/linux/amd64/ginkgo $(OUTPUTDIR)/
 	cp $(REPODIR)/_output/local/bin/linux/amd64/e2e.test $(OUTPUTDIR)/
+	cp $(REPODIR)/build/pause/pause $(OUTPUTDIR)/
