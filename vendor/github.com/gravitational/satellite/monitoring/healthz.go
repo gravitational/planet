fb1894a30a6fa981df729d78acbedbeb6f5167ed
dmitri <deemok@gmail.com>
2016-03-18 16:53:13 +0100

    Bumping k8s version to 1.2

--- vendor/github.com/gravitational/satellite/monitoring/healthz.go
+++ vendor/github.com/gravitational/satellite/monitoring/healthz.go
@@ -1,9 +1,25 @@
+/*
+Copyright 2016 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package monitoring
 
 import (
 	"io"
 	"net"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gravitational/satellite/agent/health"
@@ -36,7 +52,13 @@ func (r *HttpHealthzChecker) Check(reporter health.Reporter) {
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		reporter.Add(NewProbeFromErr(r.name, trace.Errorf("unexpected HTTP status: %s", http.StatusText(resp.StatusCode))))
+		reporter.Add(&pb.Probe{
+			Checker: r.name,
+			Status:  pb.Probe_Failed,
+			Error: trace.Errorf("unexpected HTTP status: %s",
+				http.StatusText(resp.StatusCode)).Error(),
+			Code: strconv.Itoa(resp.StatusCode),
+		})
 		return
 	}
 	if err = r.checker(resp.Body); err != nil {
