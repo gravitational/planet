67ef2d35fdeabf2683845a1c229584d495da7a38
Sasha Klizhentas <klizhentas@gmail.com>
2016-10-01 18:55:25 -0700

    use external CIDR

--- tool/planet/cfg.go
+++ tool/planet/cfg.go
@@ -11,7 +11,6 @@ import (
 
 	kv "github.com/gravitational/configure"
 	"github.com/gravitational/configure/cstrings"
-	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 type Config struct {
@@ -31,8 +30,8 @@ type Config struct {
 	StateDir                string
 	DockerBackend           string
 	DockerOptions           string
-	ServiceSubnet           CIDR
-	PODSubnet               CIDR
+	ServiceSubnet           kv.CIDR
+	PODSubnet               kv.CIDR
 	InitialCluster          kv.KeyVal
 	ServiceUser             *user.User
 	ServiceUID              string
@@ -65,62 +64,6 @@ func (l *list) Set(val string) error {
 
 func (l *list) String() string {
 	return fmt.Sprintf("%v", []string(*l))
-}
-
-func CIDRFlag(s kingpin.Settings) *CIDR {
-	vars := new(CIDR)
-	s.SetValue(vars)
-	return vars
-}
-
-type CIDR struct {
-	val   string
-	ip    net.IP
-	ipnet net.IPNet
-}
-
-func (c *CIDR) Set(v string) error {
-	ip, ipnet, err := net.ParseCIDR(v)
-	if err != nil {
-		return err
-	}
-	c.val = v
-	c.ip = ip
-	c.ipnet = *ipnet
-	return nil
-}
-
-func (c *CIDR) String() string {
-	return c.ipnet.String()
-}
-
-// FirstIP returns the first IP in this subnet that is not .0
-func (c *CIDR) FirstIP() net.IP {
-	var ip net.IP
-	for ip = IncIP(c.ip.Mask(c.ipnet.Mask)); c.ipnet.Contains(ip); IncIP(ip) {
-		break
-	}
-	return ip
-}
-
-// RelativeIP returns an IP given an offset from the first IP in the range.
-// offset starts at 0, i.e. c.RelativeIP(0) == c.FirstIP()
-func (c *CIDR) RelativeIP(offset int) net.IP {
-	var ip net.IP
-	for ip = IncIP(c.ip.Mask(c.ipnet.Mask)); c.ipnet.Contains(ip) && offset > 0; IncIP(ip) {
-		offset--
-	}
-	return ip
-}
-
-func IncIP(ip net.IP) net.IP {
-	for j := len(ip) - 1; j >= 0; j-- {
-		ip[j]++
-		if ip[j] > 0 {
-			break
-		}
-	}
-	return ip
 }
 
 // hostPort is a command line flag that understands input
