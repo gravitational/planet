59beeeab05a56d7678c5916c93fd5470e856d7d5
Sasha Klizhentas <klizhentas@gmail.com>
2016-10-07 10:31:49 -0700

    fix issue with DNS echo chamber

--- tool/planet/start.go
+++ tool/planet/start.go
@@ -392,8 +392,28 @@ func addDockerOptions(config *Config) {
 	config.Env.Append("DOCKER_OPTS", "--exec-opt native.cgroupdriver=cgroupfs", " ")
 }
 
-// addResolv adds resolv conf from the host's /etc/resolv.conf
 func addResolv(config *Config) error {
+	planetResolv := filepath.Join(config.Rootfs, "etc", "resolv.gravity.conf")
+	if err := copyResolvFile(planetResolv, []string{config.PublicIP}); err != nil {
+		return trace.Wrap(err)
+	}
+
+	resolvKubelet := filepath.Join(config.Rootfs, "etc", "resolv.kubelet.conf")
+	if err := copyResolvFile(resolvKubelet, nil); err != nil {
+		return trace.Wrap(err)
+	}
+
+	config.Mounts = append(config.Mounts, box.Mount{
+		Src:      planetResolv,
+		Dst:      "/etc/resolv.conf",
+		Readonly: true,
+	})
+
+	return nil
+}
+
+// copyResolvFile adds resolv conf from the host's /etc/resolv.conf
+func copyResolvFile(destination string, nameservers []string) error {
 	path, err := filepath.EvalSymlinks("/etc/resolv.conf")
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -410,13 +430,15 @@ func addResolv(config *Config) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	cfg.UpsertServer(config.PublicIP)
+	for _, nameserver := range nameservers {
+		cfg.UpsertServer(nameserver)
+	}
 	cfg.UpsertSearchDomain(DefaultSearchDomain)
 	cfg.Ndots = DNSNdots
 	cfg.Timeout = DNSTimeout
 
 	resolv, err := os.OpenFile(
-		filepath.Join(config.Rootfs, "etc", "resolv.gravity.conf"),
+		destination,
 		os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644,
 	)
 	if err != nil {
@@ -427,11 +449,7 @@ func addResolv(config *Config) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	config.Mounts = append(config.Mounts, box.Mount{
-		Src:      resolv.Name(),
-		Dst:      "/etc/resolv.conf",
-		Readonly: true,
-	})
+
 	return nil
 }
 
