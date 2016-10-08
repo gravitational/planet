e66c82f079e18f4bb015b06535c44cc75242baf5
Roman Tkachenko <roman@gravitational.com>
2016-10-07 20:59:15 -0700

    Remove deleted dnsmasq.sh script from Makefile

--- build.assets/makefiles/base/dns/dns.mk
+++ build.assets/makefiles/base/dns/dns.mk
@@ -5,9 +5,5 @@ all:
 	cp -af ./dnsmasq.service $(ROOTFS)/lib/systemd/system
 	ln -sf /lib/systemd/system/dnsmasq.service  $(ROOTFS)/lib/systemd/system/multi-user.target.wants/
 
-# script that allows setting up dnsmasq
-	mkdir -p $(ROOTFS)/usr/bin/scripts
-	install -m 0755 ./dnsmasq.sh $(ROOTFS)/usr/bin/scripts
-
 # script that sets up /etc/hosts and symlinks resolv.conf
 	mkdir -p $(ROOTFS)/etc/dnsmasq.d
