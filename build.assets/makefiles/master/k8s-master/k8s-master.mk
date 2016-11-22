4d47c4e460f4bbf01a18032ff2f968153788fa4f
Roman Tkachenko <roman@gravitational.com>
2016-11-21 16:34:58 -0800

    Add kubectl to node and default kube config

--- build.assets/makefiles/master/k8s-master/k8s-master.mk
+++ build.assets/makefiles/master/k8s-master/k8s-master.mk
@@ -3,9 +3,10 @@
 BINDIR:=$(ASSETDIR)/k8s-$(KUBE_VER)
 
 all: k8s-master.mk
-	@echo "\n---> Building master k8s components\n"
+	@echo "\n---> Building Kubernetes master components\n"
 	mkdir -p $(ROOTFS)/etc/kubernetes
 	cp -TRv -p rootfs/etc/kubernetes $(ROOTFS)/etc/kubernetes
+	cp -TRv -p rootfs/root $(ROOTFS)/root
 	cp -af ./kube-apiserver.service $(ROOTFS)/lib/systemd/system
 	cp -af ./kube-controller-manager.service $(ROOTFS)/lib/systemd/system
 	cp -af ./kube-scheduler.service $(ROOTFS)/lib/systemd/system
