01d1d74120096c534dc1f79a411e4ebfb4a888f0
dmitri <deemok@gmail.com>
2017-01-18 21:05:01 +0100

    Docker: download tarball/extract binaries in separate targets

--- build.assets/makefiles/base/docker/docker.mk
+++ build.assets/makefiles/base/docker/docker.mk
@@ -9,20 +9,22 @@ OS := Linux
 VER := 1.12.6
 override DIR := $(ASSETDIR)/docker-$(VER)
 BINARIES := \
-	$(DIR)/docker \
-	$(DIR)/docker-containerd \
-	$(DIR)/docker-containerd-ctr \
-	$(DIR)/docker-containerd-shim \
-	$(DIR)/dockerd \
-	$(DIR)/docker-proxy \
-	$(DIR)/docker-runc
-TARBALL := $(DIR)/docker.tgz
+	docker \
+	docker-containerd \
+	docker-containerd-ctr \
+	docker-containerd-shim \
+	dockerd \
+	docker-proxy \
+	docker-runc
+ASSET_BINARIES := $(addprefix $(DIR)/, $(BINARIES))
+ROOTFS_BINARIES := $(addprefix $(ROOTFS)/usr/bin/, $(BINARIES))
+TARBALL := $(ASSETDIR)/docker-$(VER).tgz
 
 REGISTRY := apiserver:5000
 
-$(ROOTFS)/usr/bin/docker: $(BINARIES)
+$(ROOTFS_BINARIES): $(BINARIES)
 # install socket-activated metadata service
-	@echo "\n---> Installing Docker to be used with Kubernetes:\n"
+	@echo "\n---> Installing Docker:\n"
 	cp -af ./docker.service $(ROOTFS)/lib/systemd/system
 	ln -sf /lib/systemd/system/docker.service  $(ROOTFS)/lib/systemd/system/multi-user.target.wants/
 	cp -af ./docker.socket $(ROOTFS)/lib/systemd/system
@@ -34,7 +36,7 @@ $(ROOTFS)/usr/bin/docker: $(BINARIES)
 
 # copy docker from the build dir into rootfs:
 	mkdir -p $(ROOTFS)/usr/bin
-	cp $(BINARIES) $(ROOTFS)/usr/bin/docker
+	cp $(DIR)/* $(ROOTFS)/usr/bin/
 
 # that's a directory with client and server certs
 	mkdir -p $(ROOTFS)/etc/docker/certs.d/$(REGISTRY)
@@ -44,11 +46,11 @@ $(ROOTFS)/usr/bin/docker: $(BINARIES)
 	ln -sf /var/state/kubelet.key $(ROOTFS)/etc/docker/certs.d/$(REGISTRY)/client.key
 
 
-$(BINARIES):
-	tar --strip-components=1 -xvzf $(TARBALL) -C $(DIR)
-	rm $(TARBALL)
+$(BINARIES): $(TARBALL)
+	tar --strip-components=1 -xvzf $(TARBALL) -C $(DIR)/
 
 $(TARBALL):
 # download release
+	@echo "\n---> Downloading Docker:\n"
 	mkdir -p $(DIR)
 	curl -L https://get.docker.com/builds/$(OS)/$(ARCH)/docker-$(VER).tgz -o $@
