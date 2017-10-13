322620e38709b395fb80bd6da98a0d091b2ff4e8
Sergei Antipov <greendayonfire@gmail.com>
2017-10-13 20:39:23 +0700

    Updated metrics collector

--- lib/monitoring/metrics.go
+++ lib/monitoring/metrics.go
@@ -3,13 +3,13 @@ package monitoring
 import (
 	"github.com/gravitational/satellite/agent"
 	"github.com/gravitational/satellite/monitoring"
-	"github.com/gravitational/satellite/monitoring/etcd"
+	"github.com/gravitational/satellite/monitoring/collector"
 	"github.com/gravitational/trace"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 // AddMetrics exposes specific metrics to Prometheus
-func AddMetrics(config *Config) error {
+func AddMetrics(node agent.Agent, config *Config) error {
 	etcdConfig := &monitoring.ETCDConfig{
 		Endpoints: config.ETCDConfig.Endpoints,
 		CAFile:    config.ETCDConfig.CAFile,
@@ -17,46 +17,17 @@ func AddMetrics(config *Config) error {
 		KeyFile:   config.ETCDConfig.KeyFile,
 	}
 
-	var metrics []prometheus.Collector
+	var mc *collector.MetricsCollector
 	var err error
+
 	switch config.Role {
 	case agent.RoleMaster:
-		metrics, err = addMetricsToMaster(etcdConfig)
+		mc, err = collector.NewMetricsCollector(etcdConfig, config.KubeAddr, agent.RoleMaster)
 	case agent.RoleNode:
-		metrics, err = addMetricsToNode(etcdConfig)
+		mc, err = collector.NewMetricsCollector(etcdConfig, config.KubeAddr, agent.RoleNode)
 	}
-	if err != nil {
+	if err = prometheus.Register(mc); err != nil {
 		return trace.Wrap(err)
 	}
-
-	for _, metric := range metrics {
-		if err = prometheus.Register(metric); err != nil {
-			return trace.Wrap(err)
-		}
-	}
 	return nil
-}
-
-func addMetricsToMaster(etcdConfig *monitoring.ETCDConfig) ([]prometheus.Collector, error) {
-	var mc []prometheus.Collector
-
-	// ETCD stats collector
-	etcdExporter, err := etcd.NewExporter(etcdConfig)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-	mc = append(mc, etcdExporter)
-	return mc, nil
-}
-
-func addMetricsToNode(etcdConfig *monitoring.ETCDConfig) ([]prometheus.Collector, error) {
-	var mc []prometheus.Collector
-
-	// ETCD stats collector
-	etcdExporter, err := etcd.NewExporter(etcdConfig)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-	mc = append(mc, etcdExporter)
-	return mc, nil
 }
