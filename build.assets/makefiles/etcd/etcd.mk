87737be94544a15b27b847fa8ea507ad108e1393
Kevin Nisbet <kevin@gravitational.com>
2018-02-08 12:46:36 -0500

    Install both etcd2 and etcd3

--- build.assets/makefiles/etcd/etcd.mk
+++ build.assets/makefiles/etcd/etcd.mk
@@ -3,17 +3,31 @@
 ARCH := amd64
 TARGET := etcd-$(ETCD_VER)-linux-$(ARCH)
 TARGET_TARBALL := $(TARGET).tar.gz
-
 DOWNLOAD:=$(ASSETDIR)/$(TARGET_TARBALL)
+
+# Until we no longer support upgrades from etcd2, we need to install both etcd3 and etcd2
+TARGET3 := etcd-$(ETCD3_VER)-linux-$(ARCH)
+TARGET3_TARBALL := $(TARGET3).tar.gz
+DOWNLOAD3:=$(ASSETDIR)/$(TARGET3_TARBALL)
 
 all: $(DOWNLOAD)
 	@echo "\n---> Building etcd:\n"
-	cd $(ASSETDIR) && tar -xzf $(ASSETDIR)/$(TARGET_TARBALL)
+	cd $(ASSETDIR) && mkdir $(TARGET) && tar -xzf $(ASSETDIR)/$(TARGET_TARBALL) -C $(TARGET)/
 	mkdir -p $(ROOTFS)/var/etcd
-	cp -afv $(ASSETDIR)/$(TARGET)/etcd $(ROOTFS)/usr/bin
-	cp -afv $(ASSETDIR)/$(TARGET)/etcdctl $(ROOTFS)/usr/bin
+	cp -afv $(ASSETDIR)/$(TARGET)/etcd $(ROOTFS)/usr/bin/etcd-$(ETCD_VER)
+	cp -afv $(ASSETDIR)/$(TARGET)/etcdctl $(ROOTFS)/usr/bin/etcdctl-$(ETCD_VER)
 	cp -afv ./etcd.service $(ROOTFS)/lib/systemd/system/
 	ln -sf /lib/systemd/system/etcd.service  $(ROOTFS)/lib/systemd/system/multi-user.target.wants/
 
+	# ETCD3
+	cd $(ASSETDIR) && mkdir $(TARGET3) && tar -xzf $(ASSETDIR)/$(TARGET3_TARBALL) -C $(TARGET3)/
+	cp -afv $(ASSETDIR)/$(TARGET3)/etcd $(ROOTFS)/usr/bin/etcd-$(ETCD3_VER)
+	cp -afv $(ASSETDIR)/$(TARGET3)/etcdctl $(ROOTFS)/usr/bin/etcdctl-$(ETCD3_VER)
+
+	# Default to oldest supported etcd
+	ln $(ROOTFS)/usr/bin/etcd-$(ETCD_VER) $(ROOTFS)/usr/bin/etcd
+	ln $(ROOTFS)/usr/bin/etcdctl-$(ETCD_VER) $(ROOTFS)/usr/bin/etcdctl
+
 $(DOWNLOAD):
 	curl -L https://github.com/coreos/etcd/releases/download/$(ETCD_VER)/$(TARGET_TARBALL) -o $(DOWNLOAD)
+	curl -L https://github.com/coreos/etcd/releases/download/$(ETCD3_VER)/$(TARGET3_TARBALL) -o $(DOWNLOAD3)
