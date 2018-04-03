aadd7b242b08c6a7d0748096b29755dfe86d8d38
dmitri <deemok@gmail.com>
2018-04-03 11:31:09 +0200

    Add support for Ubuntu Core for user lookup.

--- tool/planet/cfg_test.go
+++ tool/planet/cfg_test.go
@@ -40,8 +40,8 @@ func (r *CommandFlagSuite) TestConvertsToEtcdPeer(c *check.C) {
 	addrs := toEtcdPeerList(result)
 
 	// validate
-	expected := "172.168.178.1.example.com=http://172.168.178.1:2380,172.168.178.2.example.com=http://172.168.178.2:2380"
-	expectedReverse := "172.168.178.2.example.com=http://172.168.178.2:2380,172.168.178.1.example.com=http://172.168.178.1:2380"
+	expected := "172.168.178.1.example.com=https://172.168.178.1:2380,172.168.178.2.example.com=https://172.168.178.2:2380"
+	expectedReverse := "172.168.178.2.example.com=https://172.168.178.2:2380,172.168.178.1.example.com=https://172.168.178.1:2380"
 	c.Assert(addrs, OneOfEquals, []string{expected, expectedReverse})
 }
 
@@ -64,5 +64,9 @@ func (r *oneOfChecker) Check(params []interface{}, names []string) (result bool,
 	}()
 	actual := params[0].(string)
 	expected := params[1].([]string)
-	return actual == expected[0] || actual == expected[1], ""
+	result = actual == expected[0] || actual == expected[1]
+	if !result {
+		return false, "No match"
+	}
+	return true, ""
 }
