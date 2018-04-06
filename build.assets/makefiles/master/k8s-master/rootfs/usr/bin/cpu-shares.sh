23c1d5fff3dcbd3117e77467120d41234de31683
Kevin Nisbet <kevin@gravitational.com>
2018-04-06 17:38:05 -0400

    fix comment

--- build.assets/makefiles/master/k8s-master/rootfs/usr/bin/cpu-shares.sh
+++ build.assets/makefiles/master/k8s-master/rootfs/usr/bin/cpu-shares.sh
@@ -14,7 +14,6 @@ done
 # wait a couple second after the cgroup is created, just incase we might race kubernetes
 sleep 5
 
-# Ignore the output, incase
 echo "Setting cgroup /sys/fs/cgroup/cpu/kubepods/ cpu.shares = 2"
 echo 2 > /sys/fs/cgroup/cpu/kubepods/cpu.shares
 echo "Setting cgroup /sys/fs/cgroup/blkio/kubepods/ blkio.weight = 100"
