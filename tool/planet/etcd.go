4cea0eea596330282334cee64588aa21bbc5fece
Kevin Nisbet <kevin@gravitational.com>
2018-08-29 11:46:53 -0400

    fix is-member path

--- tool/planet/etcd.go
+++ tool/planet/etcd.go
@@ -48,7 +48,7 @@ func etcdPromote(name, initialCluster, initialClusterState string) error {
 	// Hack:
 	// persist the promotion to an empty file, so that if planet is restarted
 	// it can detect and override incorrect proxy setting passed by planet
-	f, err := os.OpenFile(DefaultEtcdIsMemberFile, os.O_RDONLY|os.O_CREATE, 0666)
+	f, err := os.OpenFile(DefaultEtcdIsMemberPath, os.O_RDONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return trace.Wrap(err)
 	}
