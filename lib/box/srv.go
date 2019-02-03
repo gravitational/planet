0d92fa77b8838c7d338fa8252a5b9ae0bca7d020
Kevin Nisbet <kevin@gravitational.com>
2019-02-03 00:42:36 -0500

    set planet container systemd path

--- lib/box/srv.go
+++ lib/box/srv.go
@@ -26,6 +26,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -320,8 +321,9 @@ func getLibcontainerConfig(containerID, rootfs string, cfg Config) (*configs.Con
 			},
 		},
 		Cgroups: &configs.Cgroup{
-			Name:   containerID,
-			Parent: "system",
+			//Name:   containerID,
+			//Parent: "system",
+			Path: path.Join("gravity", "planet", containerID),
 			Resources: &configs.Resources{
 				AllowAllDevices:  &allowAllDevices,
 				AllowedDevices:   configs.DefaultAllowedDevices,
