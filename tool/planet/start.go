97198a3766c54d3f379ac16f2ff04c5ea2cdff70
Kevin Nisbet <kevin@gravitational.com>
2019-02-03 23:32:22 -0500

    try forcing systemd to run in system mode

--- tool/planet/start.go
+++ tool/planet/start.go
@@ -245,7 +245,7 @@ func start(config *Config, monitorc chan<- bool) (*runtimeContext, error) {
 		Devices:      config.Devices,
 		DataDir:      "/var/run/planet",
 		InitUser:     "root",
-		InitArgs:     []string{"/bin/systemd"},
+		InitArgs:     []string{"/bin/systemd", "--system"},
 		InitEnv:      []string{"container=docker", "LC_ALL=en_US.UTF-8"},
 		Capabilities: allCaps,
 	}
