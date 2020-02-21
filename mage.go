d38dcc5cd72c99a3c15efe1a34da247838d343d3
Kevin Nisbet <kevin@gravitational.com>
2020-02-21 04:22:29 +0000

    fix mage.go ignore line

--- mage.go
+++ mage.go
@@ -1,3 +1,4 @@
+// +build ignore
 /*
 Copyright 2020 Gravitational, Inc.
 
@@ -13,8 +14,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-// +build ignore
 
 package main
 
