17fe2c2a341e007367c4eddfe27af3d7f1ad07a3
Bernard Kim <bernard@gravitational.com>
2020-05-18 18:02:04 -0700

    Re-enable nethealth check (#639)

--- lib/monitoring/checkers.go
+++ lib/monitoring/checkers.go
@@ -268,7 +268,13 @@ func addToMaster(node agent.Agent, config *Config, etcdConfig *monitoring.ETCDCo
 		node.AddChecker(monitoring.NewAWSHasProfileChecker())
 	}
 
-	// TODO: reenable nethealth checker once issues are fixed
+	nethealthChecker, err := monitoring.NewNethealthChecker(
+		monitoring.NethealthConfig{
+			NodeName:   config.NodeName,
+			KubeConfig: &kubeConfig,
+		},
+	)
+	node.AddChecker(nethealthChecker)
 
 	return nil
 }
@@ -321,7 +327,13 @@ func addToNode(node agent.Agent, config *Config, etcdConfig *monitoring.ETCDConf
 		node.AddChecker(monitoring.NewAWSHasProfileChecker())
 	}
 
-	// TODO: reenable nethealth checker once issues are fixed
+	nethealthChecker, err := monitoring.NewNethealthChecker(
+		monitoring.NethealthConfig{
+			NodeName:   config.NodeName,
+			KubeConfig: &nodeConfig,
+		},
+	)
+	node.AddChecker(nethealthChecker)
 
 	return nil
 }
