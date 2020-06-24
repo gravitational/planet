6746aae4252f20b68e5cee70b46720045cb510f9
dmitri <deemok@gmail.com>
2020-06-24 13:29:56 +0200

    Remove previous flannel.null prior to creating the bridge

--- build.assets/makefiles/base/network/wait-for-flannel.sh
+++ build.assets/makefiles/base/network/wait-for-flannel.sh
@@ -2,7 +2,7 @@
 source /etc/container-environment
 
 until [ -e /run/flannel/subnet.env ]; 
-  do echo \"waiting for flannel to start.\"; 
+  do echo \"waiting for flannel to start.\";
   sleep 3; 
 done; 
 
@@ -11,6 +11,7 @@ done;
 # This prevents "no route to host" errors when trying to reach the service subnet
 # by creating a dummy interface, and routing the service subnet to this interface
 # we can guarantee that the route exists, and can be NAT to the correct destination
+ip link del flannel.null || true
 ip link add flannel.null type dummy
 ip link set flannel.null up
 ip route add ${KUBE_SERVICE_SUBNET} dev flannel.null
