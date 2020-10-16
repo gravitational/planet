3161862afa578b2536b749a98e57cc07ffa5d7d0
Bernard Kim <bernard@gravitational.com>
2020-10-16 11:53:09 -0700

    Add default namespaces

--- tool/planet/constants.go
+++ tool/planet/constants.go
@@ -428,6 +428,10 @@ const (
 
 	// DefaultServiceNodePortRange defines the default IP range for services with NodePort visibility
 	DefaultServiceNodePortRange = "30000-32767"
+
+	// DefaultCriticalNamespaces is the default list of critical namespaces
+	DefaultCriticalNamespaces = "kube-system,monitoring"
+
 	// DNSEnvFile specifies the file location to write information about the overlay network
 	// in use to be picked up by scripts
 	DNSEnvFile = "/run/dns.env"
