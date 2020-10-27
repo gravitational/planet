b2eb130fd578a3461daf6b162e64dd37e9339aab
Lenko <lenko@gravitational.com>
2020-10-27 17:14:30 -0500

    Removed the deletion of /lib/modules-load.d/open-iscsi.conf  from the rootfs.

--- build.assets/makefiles/shrink-rootfs.mk
+++ build.assets/makefiles/shrink-rootfs.mk
@@ -19,5 +19,4 @@ all:
 	rm -rf $(ROOTFS)/var/log/*
 	rm -rf $(ROOTFS)/var/cache
 	rm -rf $(ROOTFS)/lib/systemd/system/sysinit.target.wants/proc-sys-fs-binfmt_misc.automount
-	rm -rf $(ROOTFS)/lib/modules-load.d/open-iscsi.conf
 	rm -rf $(ROOTFS)/usr/share/locale
