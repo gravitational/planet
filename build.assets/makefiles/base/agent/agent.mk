288b53938973020b9ee75114440608d7ec84ea17
Bernard Kim <bernard@goteleport.com>
2021-02-02 14:20:36 -0800

    Fix agent.mk

--- build.assets/makefiles/base/agent/agent.mk
+++ build.assets/makefiles/base/agent/agent.mk
@@ -1,8 +1,7 @@
 .PHONY: all
 
-all: agent.mk $(BINARIES)
-
-$(BINARIES): planet-agent.service
+all:
 	@echo "\n---> Installing services for Planet agent:\n"
+
 	cp -af ./planet-agent.service $(ROOTFS)/lib/systemd/system
 	ln -sf /lib/systemd/system/planet-agent.service  $(ROOTFS)/lib/systemd/system/multi-user.target.wants/
