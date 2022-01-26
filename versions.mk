b7dd8a14d93fa51c86c539cf15c72d6190c82397
Kevin Nisbet <kevin@gravitational.com>
2022-01-26 05:09:03 +0000

    update flannel fork: https://github.com/gravitational/flannel/pull/11

--- versions.mk
+++ versions.mk
@@ -3,7 +3,7 @@ KUBE_VER ?= v1.19.15
 SECCOMP_VER ?= 2.3.1-2.1+deb9u1
 DOCKER_VER ?= 20.10.7
 # we currently use our own flannel fork: gravitational/flannel
-FLANNEL_VER ?= v0.10.5-gravitational
+FLANNEL_VER ?= v0.10.6-gravitational
 HELM_VER ?= 2.16.12
 HELM3_VER ?= 3.3.4
 COREDNS_VER ?= 1.7.0
